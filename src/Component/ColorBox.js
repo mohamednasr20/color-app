e941072823f1037f9b3cec1394efd388bd60a076
mohamednasr20 <mohamednasr@mohameds-Air.home>
2020-12-01 13:26:41 -0500

    init app

---
+++ src/Component/ColorBox.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "../Css/ColorBox.css";
+
+const ColorBox = ({ name, background }) => {
+  const [copied, setCopied] = useState(false);
+
+  const changeCopyState = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+  return (
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
+      <div style={{ background: background }} className="ColorBox">
+        <div
+          style={{ background: background }}
+          className={`copy-overlay ${copied && "show"}`}
+        />
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>copied!</h1>
+          <p>{background}</p>
+        </div>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
+      </div>
+    </CopyToClipboard>
+  );
+};
+
+export default ColorBox;
---
+++ src/Component/Palette.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ColorBox from "./ColorBox";
+import "../Css/Palette.css";
+
+const Palette = ({ colors }) => {
+  const colorBoxes = colors.map((color) => (
+    <ColorBox background={color.color} name={color.name} />
+  ));
+
+  return (
+    <div className="Palette">
+      <div className="Palette-colors">{colorBoxes}</div>
+    </div>
+  );
+};
+
+export default Palette;
