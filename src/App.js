095700a4fabdc370303d7fb6bab80dcec5d4d123
mohamednasr20 <mohamednasr@mohameds-Air.home>
2020-12-01 14:56:13 -0500

    add color slide

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import SeedColors from "./Component/SeedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
-  console.log(generatePalette(SeedColors[4]));
   return (
     <div className="App">
-      <Palette {...SeedColors[1]} />
+      <Palette palette={generatePalette(SeedColors[4])} />
     </div>
   );
 }
--- src/Component/Palette.js
+++ src/Component/Palette.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorBox from "./ColorBox";
 import "../Css/Palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
-const Palette = ({ colors }) => {
-  const colorBoxes = colors.map((color) => (
-    <ColorBox background={color.color} name={color.name} />
+const Palette = ({ palette }) => {
+  const [level, setLevel] = useState(500);
+  const colorBoxes = palette.colors[level].map((color) => (
+    <ColorBox background={color.hex} name={color.name} />
   ));
+
+  const onLevelChange = (newLevel) => {
+    setLevel(newLevel);
+  };
 
   return (
     <div className="Palette">
+      <Slider
+        defaultValue={level}
+        min={100}
+        max={900}
+        step={100}
+        onAfterChange={onLevelChange}
+      />
       <div className="Palette-colors">{colorBoxes}</div>
     </div>
   );
