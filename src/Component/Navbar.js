1ca195fdfb2769cdd737ec60a83f445e7f96eaea
mohamednasr20 <mohamednasr@mohameds-Air.home>
2020-12-03 22:00:55 -0500

    add snack bar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -2,15 +2,28 @@ import React, { useState } from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 import "rc-slider/assets/index.css";
 import "../style/Navbar.css";
 
 const Navbar = ({ level, changeLevel, changeFormat }) => {
   const [format, setFormat] = useState("hex");
+  const [open, setOpen] = useState(false);
 
-  const handleChange = (e) => {
+  const handleFormatChange = (e) => {
     setFormat(e.target.value);
     changeFormat(e.target.value);
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
   };
   return (
     <header className="Navbar">
@@ -30,12 +43,35 @@ const Navbar = ({ level, changeLevel, changeFormat }) => {
         </div>
       </div>
       <div className="select-container">
-        <Select value={format} onChange={handleChange}>
+        <Select value={format} onChange={handleFormatChange}>
           <MenuItem value="hex">HEX -#ffffff</MenuItem>
           <MenuItem value="rgb">RGB-rgb(255, 255, 255)</MenuItem>
           <MenuItem value="rgba">RGBA-rgba(255, 255, 255, 1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        open={open}
+        autoHideDuration={3000}
+        message={
+          <span id="message-id">Format Changed To {format.toUpperCase()}</span>
+        }
+        onClose={handleClose}
+        ContentProps={{ "aria-describedby": "message-id" }}
+        action={[
+          <IconButton
+            onClick={handleClose}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
     </header>
   );
 };
